3910c7ea07c6ece7d4d5d027825e909c0177f580
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2023-04-13 14:37:42 -0400

    update timed datasets

--- R/causal_quartet_time.R
+++ R/causal_quartet_time.R
@@ -7,9 +7,8 @@
 #'   * (3) A mediator
 #'   * (4) M-bias
 #'
-#' There are three time points:
+#' There are two time points:
 #'   * baseline
-#'   * index (time 0, time of exposure classification)
 #'   * follow up
 #'
 #' These datasets help demonstrate that a model that includes only pre-exposure
@@ -22,23 +21,22 @@
 #' @examples
 #'
 #' ## incorrect model because covariate is post-treatment
-#' lm(outcome_followup ~ exposure_index + covariate_followup,
+#' lm(outcome_followup ~ exposure_baseline + covariate_followup,
 #'    data = causal_collider_time)
 #'
 #' ## correct model because covariate is pre-treatment
 #' ## even though the true mechanism dictates that the covariate is a collider,
 #' ## because the pre-exposure variable is used, the collider bias does not
 #' ## occur.
-#' lm(outcome_followup ~ exposure_index + covariate_baseline,
+#' lm(outcome_followup ~ exposure_baseline + covariate_baseline,
 #'    data = causal_collider_time)
 #' @references D'Agostino McGowan L, Barrett M (2023). Causal inference is not a statistical problem. Preprint arXiv:2304.02683v1.
 #'
 #' @format `causal_collider_time`: A dataframe with 100 rows and 7 variables:
 #'
-#' * `covariate_baseline`: known factor measured at baseline (pre-exposure)
-#' * `exposure_index`: exposure measured at the index visit (time 0)
-#' * `outcome_index`: outcome measured at the index visit (time 0)
-#' * `covariate_index`: known factor measured at the index visit (time 0)
+#' * `covariate_baseline`: known factor measured at baseline
+#' * `exposure_baseline`: exposure measured at baseline
+#' * `outcome_baseline`: outcome measured at baseline
 #' * `exposure_followup`: exposure measured at the followup visit (final time)
 #' * `outcome_followup`: outcome measured at the followup visit (final time)
 #' * `covariate_followup`: known factor measured at the followup visit (final time)
@@ -47,10 +45,9 @@
 #' @rdname causal_collider_time
 #' @format `causal_confounding_time`: A dataframe with 100 rows and 7 variables:
 #'
-#' * `covariate_baseline`: known factor measured at baseline (pre-exposure)
-#' * `exposure_index`: exposure measured at the index visit (time 0)
-#' * `outcome_index`: outcome measured at the index visit (time 0)
-#' * `covariate_index`: known factor measured at the index visit (time 0)
+#' * `covariate_baseline`: known factor measured at baseline
+#' * `exposure_baseline`: exposure measured at baseline
+#' * `outcome_baseline`: outcome measured at baseline
 #' * `exposure_followup`: exposure measured at the followup visit (final time)
 #' * `outcome_followup`: outcome measured at the followup visit (final time)
 #' * `covariate_followup`: known factor measured at the followup visit (final time)
@@ -60,10 +57,12 @@
 #' @rdname causal_collider_time
 #' @format `causal_mediator_time`: A dataframe with 100 rows and 7 variables:
 #'
-#' * `covariate_baseline`: known factor measured at baseline (pre-exposure)
-#' * `exposure_index`: exposure measured at the index visit (time 0)
-#' * `outcome_index`: outcome measured at the index visit (time 0)
-#' * `covariate_index`: known factor measured at the index visit (time 0)
+#' * `covariate_baseline`: known factor measured at baseline
+#' * `exposure_baseline`: exposure measured at baseline
+#' * `outcome_baseline`: outcome measured at baseline
+#' * `covariate_mid`: known factor measured at some mid-point
+#' * `exposure_mid`: exposure measured at some mid-point
+#' * `outcome_mid`: outcome measured at some mid-point
 #' * `exposure_followup`: exposure measured at the followup visit (final time)
 #' * `outcome_followup`: outcome measured at the followup visit (final time)
 #' * `covariate_followup`: known factor measured at the followup visit (final time)
@@ -74,11 +73,13 @@
 #'
 #' * `u1`: unmeasured factor
 #' * `u2`: unmeasured factor
-#' * `covariate_baseline`: known factor measured at baseline (pre-exposure)
-#' * `exposure_index`: exposure measured at the index visit (time 0)
-#' * `outcome_index`: outcome measured at the index visit (time 0)
-#' * `covariate_index`: known factor measured at the index visit (time 0)
+#' * `covariate_baseline`: known factor measured at baseline
+#' * `exposure_baseline`: exposure measured at baseline
+#' * `outcome_baseline`: outcome measured at baseline
 #' * `exposure_followup`: exposure measured at the followup visit (final time)
 #' * `outcome_followup`: outcome measured at the followup visit (final time)
 #' * `covariate_followup`: known factor measured at the followup visit (final time)
 "causal_m_bias_time"
+
+#' @rdname causal_collider_time
+"causal_quartet_time"
--- data-raw/generate-causal-data.R
+++ data-raw/generate-causal-data.R
@@ -57,68 +57,77 @@ causal_quartet <- bind_rows(causal_confounding,
 
 usethis::use_data(causal_quartet, overwrite = TRUE)
 
-set.seed(44)
+set.seed(93)
 
 causal_confounding_time <- tibble(
-  covariate_baseline = causal_confounding$covariate,
-  exposure_index = causal_confounding$exposure,
-  outcome_index = causal_confounding$outcome,
-  covariate_index = covariate_baseline + rnorm(n),
-  exposure_followup = exposure_index + covariate_index + rnorm(n),
-  outcome_followup = 0.5 * exposure_index + covariate_index + rnorm(n),
-  covariate_followup = covariate_index + rnorm(n),
-)
+  covariate_prebaseline = rnorm(n),
+  covariate_baseline = covariate_prebaseline + rnorm(n),
+  exposure_baseline = covariate_prebaseline + rnorm(n),
+  outcome_baseline = covariate_prebaseline + rnorm(n),
+  covariate_followup = covariate_baseline + rnorm(n),
+  exposure_followup = exposure_baseline + covariate_baseline + rnorm(n),
+  outcome_followup = 0.5 * exposure_baseline + covariate_baseline + rnorm(n),
+) |>
+  select(-covariate_prebaseline)
 
 usethis::use_data(causal_confounding_time, overwrite = TRUE)
 
-set.seed(45652)
+set.seed(1023780)
 causal_collider_time <- tibble(
-  exposure_prebaseline = rnorm(n, sd = sqrt(1/3)),
+  exposure_prebaseline = rnorm(n, sd = sqrt(1/2)),
   outcome_prebaseline = exposure_prebaseline + rnorm(n),
-  exposure_baseline = exposure_prebaseline + rnorm(n, sd = sqrt(1/3)),
+  exposure_baseline = exposure_prebaseline + rnorm(n, sd = sqrt(1/2)),
   outcome_baseline = exposure_prebaseline + rnorm(n),
   covariate_baseline = 0.45 * exposure_prebaseline + 0.77 * outcome_baseline + rnorm(n),
-  exposure_index = exposure_baseline + rnorm(n, sd = sqrt(1/3)),
-  outcome_index = exposure_baseline + rnorm(n),
-  covariate_index = 0.45 * exposure_baseline + 0.77 * outcome_index + rnorm(n),
-  exposure_followup = exposure_index + rnorm(n),
-  outcome_followup = exposure_index + rnorm(n),
-  covariate_followup = 0.45 * exposure_index + 0.77 * outcome_followup + rnorm(n)
+  exposure_followup = exposure_baseline + rnorm(n),
+  outcome_followup = exposure_baseline + rnorm(n),
+  covariate_followup = 0.45 * exposure_baseline + 0.77 * outcome_followup + rnorm(n)
 ) |>
-  select(-exposure_prebaseline, -outcome_prebaseline,
-         -exposure_baseline, -outcome_baseline)
+  select(-exposure_prebaseline, -outcome_prebaseline)
 
 usethis::use_data(causal_collider_time, overwrite = TRUE)
 
-set.seed(35396)
+set.seed(8734)
 causal_mediator_time <- tibble(
-  exposure_baseline = rnorm(n, sd = sqrt(1/2)),
-  covariate_baseline = exposure_baseline + rnorm(n),
-  exposure_index = exposure_baseline + rnorm(n, sd = sqrt(1/2)),
-  covariate_index = exposure_index + rnorm(n),
-  outcome_index = covariate_baseline + rnorm(n),
-  exposure_followup = exposure_index + rnorm(n),
-  covariate_followup = exposure_followup + rnorm(n),
-  outcome_followup = covariate_index + rnorm(n)
-) |>
-  select(-exposure_baseline)
+  exposure_baseline = rnorm(n),
+  covariate_baseline = rnorm(n),
+  outcome_baseline = rnorm(n),
+  exposure_mid = exposure_baseline + rnorm(n),
+  covariate_mid = exposure_baseline + rnorm(n),
+  outcome_mid = covariate_baseline + rnorm(n),
+  exposure_followup = exposure_mid + rnorm(n),
+  covariate_followup = exposure_mid + rnorm(n),
+  outcome_followup = covariate_mid + rnorm(n)
+)
 
 usethis::use_data(causal_mediator_time, overwrite = TRUE)
 
-set.seed(20213)
+set.seed(68392)
 causal_m_bias_time <- tibble(
   u1 = rnorm(n),
   u2 = rnorm(n),
-  exposure_baseline = u1 + rnorm(n),
-  covariate_baseline = 8 * u1 + u2 + rnorm(n),
-  exposure_index = u1 + rnorm(n),
-  outcome_index = exposure_baseline + rnorm(n),
-  covariate_index = 8 * u1 + u2 + rnorm(n),
-  outcome_followup = exposure_index + u2 + rnorm(n),
-  exposure_followup = u1 + rnorm(n),
-  covariate_followup = 8 * u1 + u2 + rnorm(n)
+  exposure_prebaseline = u1 + rnorm(n),
+  covariate_prebaseline = 8 * u1 + u2 + rnorm(n),
+  exposure_baseline = exposure_prebaseline + u1 + rnorm(n),
+  outcome_baseline = exposure_prebaseline + rnorm(n),
+  covariate_baseline = covariate_prebaseline + 8 * u1 + u2 + rnorm(n),
+  outcome_followup = exposure_baseline + u2 + rnorm(n),
+  exposure_followup = exposure_baseline + u1 + rnorm(n),
+  covariate_followup = covariate_baseline + 8 * u1 + u2 + rnorm(n)
 ) |>
-  select(-exposure_baseline)
+  select(-exposure_prebaseline, -covariate_prebaseline)
 
 usethis::use_data(causal_m_bias_time, overwrite = TRUE)
 
+
+causal_quartet_time <- bind_rows(causal_confounding_time,
+                                 causal_collider_time,
+                                 causal_mediator_time,
+                                 causal_m_bias_time) %>%
+  mutate(dataset = rep(c("(2) Confounder",
+                         "(1) Collider",
+                         "(3) Mediator",
+                         "(4) M-Bias"), each = 100)) |>
+  arrange(dataset)
+
+usethis::use_data(causal_quartet_time, overwrite = TRUE)
